4c796b7c17ea83c0c6b7212df756dfacf066827c
中村泰輔 <t.nakamura@azmore.jp>
2020-04-09 10:41:47 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React,{ Component } from 'react';
+
+const App = () => {
+  return <Counter />
+}
+class Counter extends Component
+  {
+    constructor(props)
+    {
+      super(props);
+      this.state = {
+        value : 0
+      }
+    }
+
+    onIncrement = () =>{
+      this.setState({
+        value : this.state.value + 1
+      });
+    }
+    onDecrement = () =>{
+      this.setState({
+        value : this.state.value - 1
+      });
+    }
+    render()
+    {
+      return(
+        <div>
+          <div>
+          カウント値：{this.state.value}
+          </div>
+
+          <div>
+          <button type="button" onClick={this.onIncrement}>+</button>
+          <button type="button" onClick={this.onDecrement}>-</button>
+          </div>
+        </div>
+
+
+      );
+    }
+
+  }
+
+
+
+export default App;
